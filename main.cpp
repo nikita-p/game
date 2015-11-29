d1d871d6f3a5838862e49685cbf420c4427e17a4
nikita-p <nikita.petrov.1997@gmail.com>
2015-11-29 18:13:51 +0600

    SFML

--- main.cpp
+++ main.cpp
@@ -1,13 +1,48 @@
-#include <FL/Fl.H>
-#include <FL/Fl_Window.H>
-#include <FL/Fl_Button.H>
-using namespace std;
+#include <SFML/Graphics.hpp>
 
-int main() {
-    Fl_Window win( 500,300,"Test window" );
-    win.begin();
-       Fl_Button but( 10, 150, 70, 30,"Click" );
-    win.end();
-    win.show();
-    return Fl::run();
+using namespace sf;
+
+int main()
+{
+    RenderWindow window(VideoMode(600,600),"Test");
+    CircleShape MyCircle1(30,170);
+    CircleShape MyCircle2(50,50);
+    MyCircle1.setFillColor(Color::Green);
+    MyCircle2.setFillColor(Color::Blue);
+    MyCircle1.move(70,100);
+    Clock clock;
+
+    while(window.isOpen())
+    {
+        float time =clock.getElapsedTime().asMicroseconds();
+        clock.restart();
+        Event event;
+        while(window.pollEvent(event))
+        {
+            if(event.type == Event::Closed)
+                window.close();
+        }
+
+        if(Keyboard::isKeyPressed(Keyboard::Left))
+        {
+            MyCircle1.move(-0.001*time,0);
+        }
+        if(Keyboard::isKeyPressed(Keyboard::Right))
+        {
+            MyCircle1.move(0.001*time,0);
+        }
+        if(Keyboard::isKeyPressed(Keyboard::Up))
+        {
+            MyCircle1.move(0,-0.001*time);
+        }
+        if(Keyboard::isKeyPressed(Keyboard::Down))
+        {
+            MyCircle1.move(0,0.001*time);
+        }
+        window.clear();
+        window.draw(MyCircle1);
+        window.draw(MyCircle2);
+        window.display();
+    }
+    return 0;
 }
