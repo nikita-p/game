a4a28b4283946a024501328902a5a2e06eb6a888
nikita-p <nikita.petrov.1997@gmail.com>
2015-11-30 17:23:15 +0600

    new commit

--- main.cpp
+++ main.cpp
@@ -4,44 +4,22 @@ using namespace sf;
 
 int main()
 {
-    RenderWindow window(VideoMode(600,600),"Test");
-    CircleShape MyCircle1(30,170);
-    CircleShape MyCircle2(50,50);
-    MyCircle1.setFillColor(Color::Green);
-    MyCircle2.setFillColor(Color::Blue);
-    MyCircle1.move(70,100);
-    Clock clock;
-
+    RenderWindow window(VideoMode(600,600),"click_moving");
+    CircleShape Circle(300,70);
+    Circle.setFillColor(Color::Red);
+    Circle.setPosition(40,50);
     while(window.isOpen())
     {
-        float time =clock.getElapsedTime().asMicroseconds();
-        clock.restart();
         Event event;
         while(window.pollEvent(event))
         {
             if(event.type == Event::Closed)
                 window.close();
-        }
-
-        if(Keyboard::isKeyPressed(Keyboard::Left))
-        {
-            MyCircle1.move(-0.001*time,0);
-        }
-        if(Keyboard::isKeyPressed(Keyboard::Right))
-        {
-            MyCircle1.move(0.001*time,0);
-        }
-        if(Keyboard::isKeyPressed(Keyboard::Up))
-        {
-            MyCircle1.move(0,-0.001*time);
-        }
-        if(Keyboard::isKeyPressed(Keyboard::Down))
-        {
-            MyCircle1.move(0,0.001*time);
+            if(event.type==Event::MouseButtonPressed)
+                Circle.move(10,0);
         }
         window.clear();
-        window.draw(MyCircle1);
-        window.draw(MyCircle2);
+        window.draw(Circle);
         window.display();
     }
     return 0;
