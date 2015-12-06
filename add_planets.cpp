edfd0d3d6b91e150c04e2164a20f0f79c3dca6b9
nikolai9629 <nikolai96291@gmail.com>
2015-12-07 03:54:48 +0600

    Update add_planets.cpp

--- add_planets.cpp
+++ add_planets.cpp
@@ -5,18 +5,18 @@ void add_planets(RenderWindow *window, Event event, Sprite *fn_sprite, Sprite *p
 
 		
 		
-		int k = 4, A = 150;
+		int k = 4, T = 150;//T-интервал между кадрами, k-число кадров
 		
 		
 		for (int j = 0; j<k; j++)
 		{
-			if (time > A*j)
+			if (time > T*j)
 				for (int i = 0; i < N; i++)
 				{
 					int y1 = (planets[i].type - 1) * 200, y2 = y1 + 600, x = j * 200;
 					planets1_sprite[i].setTextureRect(IntRect(x, y1, 200, 200));
 					planets2_sprite[i].setTextureRect(IntRect(x, y2, 200, 200));
-					if ((time > A*k) && (i == N - 1))
+					if ((time > T*k) && (i == N - 1))
 						*timescore = 0;
 				}
 		}
