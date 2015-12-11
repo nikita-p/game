335b0d94aaa13388c6e216061862a6a3b5addcf7
nikolai9629 <nikolai96291@gmail.com>
2015-12-12 02:05:11 +0600

    Update add_planets.cpp

--- add_planets.cpp
+++ add_planets.cpp
@@ -1,5 +1,4 @@
-#include "header.h"
-void add_planets(RenderWindow *window, Event event, Sprite *fn_sprite, Sprite *planets1_sprite, Sprite *planets2_sprite, struct planet  *planets, float *timescore, int N)
+void add_planets(RenderWindow *window, Event event, Sprite *fn_sprite, Sprite *planets1_sprite, Sprite *planets2_sprite, struct planets  *list, float *timescore, int N)
 {
 	float time = *timescore;
 	int k = 8, T = 100, Lenght = 200;
@@ -8,7 +7,7 @@ void add_planets(RenderWindow *window, Event event, Sprite *fn_sprite, Sprite *p
 		if (time > T*j)
 			for (int i = 0; i < N; i++)
 			{
-				int y1 = (planets[i].type - 1) *Lenght, y2 = y1 + 3 * Lenght, x = j * Lenght;
+				int y1 = (list[i].type - 1) *Lenght, y2 = y1 + 3 * Lenght, x = j * Lenght;
 				planets1_sprite[i].setTextureRect(IntRect(x, y1, 200, 200));
 				planets2_sprite[i].setTextureRect(IntRect(x, y2, 200, 200));
 				if ((time > T*k) && (i == N - 1))
@@ -21,22 +20,22 @@ void add_planets(RenderWindow *window, Event event, Sprite *fn_sprite, Sprite *p
 		if (event.key.code == Mouse::Left)
 		{
 			for (int i = 0; i < N; i++)
-				if (event.mouseButton.x > planets[i].x && event.mouseButton.x < (planets[i].x + Lenght) && event.mouseButton.y > planets[i].y && event.mouseButton.y < (planets[i].y + Lenght))
+				if (event.mouseButton.x > list[i].x && event.mouseButton.x < (list[i].x + Lenght) && event.mouseButton.y > list[i].y && event.mouseButton.y < (list[i].y + Lenght))
 				{
 					for (int j = 0; j < N; j++)
-						planets[j].state = 0;
-					planets[i].state = 1;
+						list[j].state = 0;
+					list[i].state = 1;
 					check_position = true;
 				}
 			if (check_position == false)
 				for (int i = 0; i < N; i++)
-					planets[i].state = 0;
+					list[i].state = 0;
 		}
 	}
 	window->draw(*fn_sprite);
 	for (int i = 0; i < N; i++)
 	{
-		if (planets[i].state == 0)
+		if (list[i].state == 0)
 			window->draw(planets1_sprite[i]);
 		else
 			window->draw(planets2_sprite[i]);
